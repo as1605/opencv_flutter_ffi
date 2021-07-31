26ed77004962b7830a88be8951cb781522fba867
as1605 <1605.aditya.singh@gmail.com>
2021-07-31 13:24:05 +0530

    Restructured

--- lib/opencv-cpp/gaussian.cpp
+++ lib/opencv-cpp/gaussian.cpp
@@ -1,7 +1,7 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
-extern "C" __attribute__((visibility("default"))) __attribute__((used))
+
 void Gaussian(char *path) {
     Mat img = imread(path);
     Mat temp;
---
+++ lib/opencv-cpp/main.cpp
@@ -0,0 +1,5 @@
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+void Gaussian(char *);
+
+
+#include "gaussian.cpp"
